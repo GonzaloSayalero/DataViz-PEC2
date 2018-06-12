917e5cab23012fe068460c26e98da2c6e894b45d
GonzaloSayalero <32746279+GonzaloSayalero@users.noreply.github.com>
2018-06-12 21:12:49 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+//export class AppComponent {
+//  title = 'gonzalo';
+//}
+export class AppComponent {
+  title = 'Gonzalo';
+  private chartData: Array<any>;
+
+  //constructor(private _ApiService: ApiService) { }
+
+  ngOnInit() {
+
+    // Local Data //////////////////////////////////////////////////////////////////////
+    this.generateData();
+    // change the data periodically
+    //setInterval(() => this.generateData(), 3000);
+    // API Data ////////////////////////////////////////////////////////////////////////
+    //this._ApiService.getData().subscribe(result => { this.chartData = result })
+  }
+
+  generateData() {
+    this.chartData = [];
+    for (let i = 0; i < (8 + Math.floor(Math.random() * 10)); i++) {
+      this.chartData.push([
+        `Index ${i}`,
+        Math.floor(Math.random() * 100)
+      ]);
+    }
+  }
+}
